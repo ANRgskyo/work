40fe2e7af628704b70cc8408eaee5f22d475f22d
ANRgskyo <RAngskyo@gmail.com>
2018-12-05 20:40:52 +0900

    [Finish]3-6

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def new
-  	  @post = Post.new
+  	@post = Post.new
   end
   
   def create
@@ -8,10 +8,18 @@ class PostsController < ApplicationController
   	post = Post.new(post_params)
     # DBへの保存
   	post.save
-  	#トップ画面へリダイレクト
-  	redirect_to '/top'
+  	# 詳細画面へリダイレクト
+  	redirect_to post_path(post.id)
+  end
+
+  def index
+    @posts = Post.all
   end
   
+  def show
+  	@post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
--- vagrant/sample_app/config/routes.rb
+++ vagrant/sample_app/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/top' => 'root#top'
   post'/posts' => 'posts#create'
   get '/posts' => 'posts#index'
-  
+  get '/posts/:id' => 'posts#show', as: 'post'
+  #/posts/1や/posts/3に該当する
 end
