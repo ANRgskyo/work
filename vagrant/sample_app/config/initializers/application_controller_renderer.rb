4ac6f623d7b1418d5066b2cdac2f4ca1316b5df0
ANRgskyo <RAngskyo@gmail.com>
2018-12-06 15:52:33 +0900

    [Finish]3-8

--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3fde3d1e746e8346930eddfbfc119755440f2c05056505bb839493ccbb1925cded77c1cbff4e9e36bc6d64e469f4680777085c5ff5c9d0c528a417e1ca066b38'
